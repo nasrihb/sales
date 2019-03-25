696d4fb0f560e432e496d0e9a32e2ca495e1e94b
rihab nasri <nas.rihab12@gmail.com>
2019-03-25 10:37:41 +0100

    commit 001 :source code

---
+++ Sales/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Gestion de vente',
+    'version': '1.0',
+    'author': 'Nasri rihab',
+    'website': '',
+    'support': 'nas.rihab12@gmail.com',
+    'license': "AGPL-3",
+    'complexity': 'easy',
+    'sequence': 1,
+    'category': 'category',
+    'description': """
+        Put your description here for your module:
+            - model1
+            - model2
+            - model3
+    """,
+    'depends': ['mail'],
+    'summary': 'sales, odoo 11, erp, ',
+    'data': [
+#         'security/formation.xml',
+#         'security/ir.model.access.csv',
+#         'views/formation_views.xml',
+#         'views/formation_inherit.xml',
+#         'data/sequence.xml',
+#         'demo/demo.xml',
+#         'controllers/formation.xml',
+#         'controllers/claim.xml',
+#         'controllers/claim.xml',
+#         'report/report.xml',
+#         'report/registration.xml',
+#         'views/snippet.xml',
+#         'wizard/wiz_views.xml',
+#         'menu.xml',
+    ],
+    'demo': [
+        'demo/demo.xml'
+    ],
+    'css': [
+        #'static/src/css/ModuleName_style.css'
+    ],
+    
+    'installable': True,
+    'application': True,
+}
